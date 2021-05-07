6ac206d5c8db7d4b56e635c9cbae83865f143b58
adrienbanse <adrien.banse@gmail.com>
2021-05-07 14:55:04 +0200

    float instead of strings

--- src/test.py
+++ src/test.py
@@ -1,6 +1,7 @@
 from model import CNN
 from server import Server
 from constants import *
+import numpy as np
 
 if __name__ == "__main__":
     server = Server(nr_clients=NR_CLIENTS, lr=LR, model=CNN(), epochs=CLIENT_EPOCHS)#, is_private=True)
@@ -11,6 +12,6 @@ if __name__ == "__main__":
         print(f"Global iteration {nr_iter + 1}, test_loss: {test_loss:.4f}, test_acc: {test_acc}")
         test_losses.append(test_loss)
         test_accs.append(test_acc)
-    print(f"Test losses: {['%.4f'%loss for loss in test_losses]}")
+    print(f"Test losses: {list(np.around(np.array(test_losses),4))}")
     print(f"Test accuracies: {test_accs}")
     print("Finished")
