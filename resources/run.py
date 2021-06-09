2c0651af69514bf28de74cec008b8429dabade62
Jan Bauer <jan.bauer@kreischers.com>
2021-06-09 12:09:00 +0200

    Removed old resources

--- resources/run.py
+++
@@ -1,6 +0,0 @@
-from federated_learning.aggregation_server import AggregationServer
-from federated_learning.data_scientist import DataScientist
-
-if __name__ == '__main__':
-    data_scientist = DataScientist()
-    agg_server = AggregationServer(data_scientist.model)
